49624b0dd6dee6e6e77828f6940dc9b6151ea6a3
Andrei Tigai <andrey.tigay@gmail.com>
2019-11-13 17:49:48 +0100

    add restaurant reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all if Rails.env.development?
+
+restaurant_one = Restaurant.new({
+    name: 'First',
+    address: 'Somewhere close',
+    category: 'chinese',
+    phone_number: '23455224'
+})
+restaurant_one.save
+
+restaurant_two = Restaurant.new({
+    name: 'Second',
+    address: 'Somewhere close',
+    category: 'italian',
+    phone_number: '455665733'
+})
+restaurant_two.save
+
+restaurant_three = Restaurant.new({
+    name: 'Third',
+    address: 'Somewhere close',
+    category: 'japanese',
+    phone_number: '34366753345'
+})
+restaurant_three.save
